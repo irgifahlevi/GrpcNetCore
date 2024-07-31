25cab7176eeaf203b7160214da68b760aa9b92fb
irgifahlevi <irgifahlevi5@gmail.com>
2024-07-31 20:58:38 +0700

    add models & db context

---
+++ Models/ToDoItems.cs
@@ -0,0 +1,12 @@
+﻿namespace ToDoGrpc.Models
+{
+    public class ToDoItems
+    {
+        #region properties
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string? ToDoStatus { get; set; } = "NEW";
+        #endregion
+    }
+}
